403f953002724799e2eee4520d8de4f9600a55ee
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-11-26 21:11:34 +0200

    CLI module

--- lib/gits.rb
+++ lib/gits.rb
@@ -3,5 +3,8 @@ $LOAD_PATH.unshift __dir__ # For use/testing when no gem is installed
 module Gits
   autoload :VERSION, "gits/version"
   autoload :Error, "gits/error"
-  autoload :CLI, "gits/cli"
+
+  module CLI
+    autoload :Runner, "gits/cli/runner"
+  end
 end
