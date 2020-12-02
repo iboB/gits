b7980fbcd23ed46869fcd3d1800c6617712592a4
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-12-02 11:47:28 +0200

    parse_specs WiP

--- lib/gits/yutil.rb
+++ lib/gits/yutil.rb
@@ -3,5 +3,19 @@ require 'yaml'
 module Gits
   module YUtil
     extend self
+
+    def parse_specs(yml)
+      h = YAML.load(yml)
+      deps = h['deps']
+      raise Error.new "deps must be an array" if deps.class != Array
+      deps.map do |val|
+        val.transform_keys!(&:to_sym) if val.class == Hash
+        DepSpec.new val
+      end
+    end
+
+    def parse_specs_file(fname)
+      parse_specs File.read fname
+    end
   end
 end
--- test/test_yutil.rb
+++ test/test_yutil.rb
@@ -4,4 +4,16 @@ require 'test/unit'
 include Gits
 
 class TestYUtil < Test::Unit::TestCase
+  def test_parse_specs
+    src = <<~YAML
+      deps:
+        - https://github.com/someuser/foo.git
+        - https://github.com/other/bar.git
+        - git@github.com:alice/testlib.git
+    YAML
+
+    YUtil.parse_specs(src).tap do |specs|
+      assert_equal 3, specs.length
+    end
+  end
 end
